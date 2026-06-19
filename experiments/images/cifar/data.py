b891aa9cecb4f3f91e1d13e137290547589f1908
Lucas Grasso <lucasgrassoramos@gmail.com>
2026-06-19 01:11:15 -0300

    revert workers

--- experiments/images/cifar/data.py
+++ experiments/images/cifar/data.py
@@ -8,9 +8,7 @@ CIFAR_MEAN = (0.4914, 0.4822, 0.4465)
 CIFAR_STD = (0.2470, 0.2435, 0.2616)
 
 
-def get_loaders(
-    batch_size, subset=None, seed=0, root=".data/cifar10", num_workers=4
-):
+def get_loaders(batch_size, subset=None, seed=0, root=".data/cifar10"):
     tf = transforms.Compose(
         [
             transforms.ToTensor(),
@@ -24,7 +22,6 @@ def get_loaders(
         train = Subset(train, range(subset))
         test = Subset(test, range(min(subset, len(test))))
     g = torch.Generator().manual_seed(seed)
-    persistent = num_workers > 0
     return (
         DataLoader(
             train,
@@ -32,17 +29,11 @@ def get_loaders(
             shuffle=True,
             drop_last=True,
             generator=g,
-            num_workers=num_workers,
-            pin_memory=True,
-            persistent_workers=persistent,
         ),
         DataLoader(
             test,
             batch_size,
             shuffle=False,
             drop_last=False,
-            num_workers=num_workers,
-            pin_memory=True,
-            persistent_workers=persistent,
         ),
     )
--- experiments/images/mnist/data.py
+++ experiments/images/mnist/data.py
@@ -6,9 +6,7 @@ from torch.utils.data import DataLoader, Subset
 MNIST_MEAN, MNIST_STD = 0.1307, 0.3081
 
 
-def get_loaders(
-    batch_size, subset=None, seed=0, root=".data/mnist", num_workers=4
-):
+def get_loaders(batch_size, subset=None, seed=0, root=".data/mnist"):
     tf = transforms.Compose(
         [
             transforms.ToTensor(),
@@ -21,7 +19,6 @@ def get_loaders(
         train = Subset(train, range(subset))
         test = Subset(test, range(min(subset, len(test))))
     g = torch.Generator().manual_seed(seed)
-    persistent = num_workers > 0
     return (
         DataLoader(
             train,
@@ -29,17 +26,11 @@ def get_loaders(
             shuffle=True,
             drop_last=True,
             generator=g,
-            num_workers=num_workers,
-            pin_memory=True,
-            persistent_workers=persistent,
         ),
         DataLoader(
             test,
             batch_size,
             shuffle=False,
             drop_last=False,
-            num_workers=num_workers,
-            pin_memory=True,
-            persistent_workers=persistent,
         ),
     )
